0786f445f714f38dddc6c1084678a18496cd956a
Berriano <beresford2102@yahoo.com.au>
2021-10-18 10:24:51 +1000

    PicCardNeedFix

--- script.js
+++ script.js
@@ -13,31 +13,46 @@ let score = 0;
 const scoreArray =[5,10,15,25,50,75,125,250,500]
 
 
+// /set functionality
+const myNumFunc = myNo =>{ myNumber = myNo;
+    console.log(myNumber)};
 
-//buttons
-let aceChoice =  document.querySelector('#ace-choice');
-let twoChoice =  document.querySelector('#two-choice');
-let threeChoice =  document.querySelector('#three-choice');
-let fourChoice =  document.querySelector('#four-choice');
-let fiveChoice =  document.querySelector('#five-choice');
-let sixChoice =  document.querySelector('#six-choice');
-let sevenChoice =  document.querySelector('#seven-choice');
-let eightChoice =  document.querySelector('#eight-choice');
-let nineChoice =  document.querySelector('#nine-choice');
-let tenChoice =  document.querySelector('#ten-choice');
-let jackChoice =  document.querySelector('#jack-choice');
-let queenChoice =  document.querySelector('#queen-choice');
-let kingChoice =  document.querySelector('#king-choice');
+const scoreCardFunc = scoreText => scoreCard.textContent = scoreText;
 
-let clubChoice =  document.querySelector('#club-choice');
-let heartChoice =  document.querySelector('#heart-choice');
-let spadeChoice =  document.querySelector('#spade-choice');
-let diamondChoice =  document.querySelector('#diamond-choice');
+const matchUpFunc= match => match ===randomCardNumber && mySuit === randomCardSuit;
 
-let reset = document.querySelector('#reset');
+const querySelect =  card =>  document.querySelector(card);
+
+const suitChoice = suit => { mySuit = suit;
+console.log(mySuit)};
+
+
+//button selectors
+let aceChoice =  querySelect('#ace-choice');
+let twoChoice =  querySelect('#two-choice');
+let threeChoice =  querySelect('#three-choice');
+let fourChoice =  querySelect('#four-choice');
+let fiveChoice =  querySelect('#five-choice');
+let sixChoice =  querySelect('#six-choice');
+let sevenChoice =  querySelect('#seven-choice');
+let eightChoice =  querySelect('#eight-choice');
+let nineChoice =  querySelect('#nine-choice');
+let tenChoice =  querySelect('#ten-choice');
+let jackChoice =  querySelect('#jack-choice');
+let queenChoice =  querySelect('#queen-choice');
+let kingChoice =  querySelect('#king-choice');
+
+let clubChoice =  querySelect('#club-choice');
+let heartChoice =  querySelect('#heart-choice');
+let spadeChoice =  querySelect('#spade-choice');
+let diamondChoice =  querySelect('#diamond-choice');
+
+let reset = querySelect('#reset');
+
+
+
 
 // set random card and convert special cards
-
 let randomCardNumber = Math.trunc(Math.random()*13)+1;
 let randomCardSuit = Math.trunc(Math.random()*4)+1;
 
@@ -59,73 +74,72 @@ console.log(randomCard);
 
 
 // scoring
-
 const switchScoring = function(){ 
 
 
 if(matchUpFunc(myNumber)){
      scoreCardFunc(`Score:${score += scoreArray[8]}`);
-        console.log( `You score ${scoreArray[8]} points`);
+        // console.log( `You score ${scoreArray[8]} points`);
           
  } 
 else if(matchUpFunc(myNumber+5)){
     scoreCardFunc(`Score:${score += scoreArray[3]}`);
-     console.log( `You score ${scoreArray[3]} points`);
+    //  console.log( `You score ${scoreArray[3]} points`);
        
  }
 else if(matchUpFunc(myNumber-5)){
    scoreCardFunc( `Score:${score +=scoreArray[3]}`);
-       console.log( `You score ${scoreArray[3]} points`);
+    //    console.log( `You score ${scoreArray[3]} points`);
          
 }
 else if(matchUpFunc(myNumber-4)){
 scoreCardFunc( `Score:${score += scoreArray[4]}`);
-    console.log( `You score ${ scoreArray[4]} points`);
+    // console.log( `You score ${ scoreArray[4]} points`);
         
 }
 else if(matchUpFunc(myNumber+4)){
 scoreCard.textContent = `Score:${score += scoreArray[4]}`;
- console.log( `You score ${ scoreArray[4]} points`);
+//  console.log( `You score ${ scoreArray[4]} points`);
    
 }
 else if(matchUpFunc(myNumber-3)){
    scoreCardFunc( `Score:${score += scoreArray[5]}`);
-        console.log( `You score ${ scoreArray[5]} points`);
+        // console.log( `You score ${ scoreArray[5]} points`);
            
 }
 else if(matchUpFunc(myNumber+3)){
    scoreCardFunc( `Score:${score += scoreArray[5]}`);
-        console.log( `You score ${ scoreArray[5]} points`);
+        // console.log( `You score ${ scoreArray[5]} points`);
            
 }
 else if(matchUpFunc(myNumber-2)){
    scoreCardFunc( `Score:${score += scoreArray[6]}`);
-        console.log( `You score ${ scoreArray[6]} points`);
+        // console.log( `You score ${ scoreArray[6]} points`);
            
 }
 else if(matchUpFunc(myNumber+2)){
    scoreCardFunc( `Score:${score += scoreArray[6]}`);
-        console.log( `You score ${ scoreArray[6]} points`);
+        // console.log( `You score ${ scoreArray[6]} points`);
            
 }
 else if(matchUpFunc(myNumber-1)){
    scoreCardFunc( `Score:${score += scoreArray[7]}`);
-        console.log( `You score ${ scoreArray[7]} points`);
+        // console.log( `You score ${ scoreArray[7]} points`);
            
 }
 else if(matchUpFunc(myNumber+1)){
     scoreCardFunc( `Score:${score += scoreArray[7]}`);
-        console.log( `You score ${ scoreArray[7]} points`);
+        // console.log( `You score ${ scoreArray[7]} points`);
            
 }
 else if (myNumber === randomCardNumber && mySuit !== randomCardSuit){
     scoreCardFunc( `Score:${score +=  scoreArray[2]}`);
-        console.log( `You score ${ scoreArray[2]} points`);
+        // console.log( `You score ${ scoreArray[2]} points`);
            
 }
 else if(myNumber !== randomCardNumber && mySuit === randomCardSuit){
     scoreCardFunc( `Score:${score += scoreArray[1]}`);
-        console.log( `You score ${scoreArray[1]} points`);
+        // console.log( `You score ${scoreArray[1]} points`);
            
 }
 
@@ -133,12 +147,7 @@ else if(myNumber !== randomCardNumber && mySuit === randomCardSuit){
 } 
 
 
-  // /set functionality
-
-  const myNumFunc = myNo =>{ myNumber = myNo;
-  console.log(myNumber);};
-  const scoreCardFunc = scoreText => scoreCard.textContent = scoreText;
-  const matchUpFunc= match => match ===randomCardNumber && mySuit === randomCardSuit;
+ ;
 
 lCard.addEventListener('click', function(){
 
@@ -172,10 +181,7 @@ rCard.addEventListener('click', function(){
             break;
             case 13: myNumber = "King";
             break;
-        }
-
-
-        
+        }       
     rCard.textContent =`${myNumber} of ${mySuit}`;
 }
 else    rCard.textContent =`Please Pick a Card!!`;
@@ -184,23 +190,19 @@ else    rCard.textContent =`Please Pick a Card!!`;
 // suit choice
 
 clubChoice.addEventListener('click', function(){
-    mySuit = "Clubs";
-    console.log(mySuit);
+    suitChoice("Clubs")
     });
 
 heartChoice.addEventListener('click', function(){
-mySuit = "Hearts";
-console.log(mySuit);
+    suitChoice("Hearts")
 });
 
 spadeChoice.addEventListener('click', function(){
-mySuit = "Spades";
-console.log(mySuit);
+    suitChoice("Spades")
 })
 
 diamondChoice.addEventListener('click', function(){
-mySuit = "Diamonds";
-console.log(mySuit);
+    suitChoice("Diamonds")
 })
 
 
